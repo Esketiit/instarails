ea114c78c041ee0b46c43a3283120e532702449b
Samanthaponce5 <samanthaponce5@gmail.com>
2020-03-26 09:55:40 -0400

    post layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
         @post.account_id = current_user.id
         if @post.save
-            redirect_to root_path
+            redirect_to post_path(@post.id)
             # redirect_to account_path(current_user.id)
         else
             flash[:errors]= @post.errors.full_messages
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
     end
 
     def show
-
+        @post = Post.find(params[:id])
     end
 
     def post_params
